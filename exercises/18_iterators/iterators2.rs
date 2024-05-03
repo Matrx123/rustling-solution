ebe846d05c679996337d6938b66764dd91b6b9dd
vipin joshi <vpnjsh55@gmail.com>
2024-05-03 22:36:35 +0530

    iterators exercise

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -6,15 +6,15 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
+
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.to_uppercase().collect::<String>() + c.as_str()
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -23,7 +23,10 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-   words.iter().map(|item| capitalize_first(item)).collect()
+    words
+        .iter()
+        .map(|item| capitalize_first(item))
+        .collect()
 }
 
 // Step 3.
@@ -31,7 +34,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    let data: Vec<String> = words.iter().map(|item| capitalize_first(item)).filter(|item| item != " ").collect();
+    let data: Vec<String> = words
+        .iter()
+        .map(|item| capitalize_first(item))
+        .filter(|item| item != " ")
+        .collect();
     data.join(" ")
 }
 
